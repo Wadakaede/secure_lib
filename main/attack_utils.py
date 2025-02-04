28aa04c276182c5437791789b332097b256b2ca7
Wadakaede <kaede04079642@outlook.com>
2025-02-04 10:53:17 +0900

    Update attack_utils.py

--- main/attack_utils.py
+++ main/attack_utils.py
@@ -1,10 +1,21 @@
-import requests # type: ignore
+import requests
 
-def directory_bruteforce(url: str, wordlist: list) -> list:
+def load_wordlist_from_file(file_path):
+    with open(file_path, 'r') as file:
+        # ファイル内の各行を取り出してリストに格納
+        wordlist_from_file = [line.strip() for line in file.readlines()]
+    return wordlist_from_file
+
+def directory_bruteforce(url: str, wordlist: list, file_path: str) -> list:
+    # txtファイルからワードリストを読み込んで既存のリストに結合
+    file_wordlist = load_wordlist_from_file(file_path)
+    full_wordlist = wordlist + file_wordlist  # 既存のリストと結合
+    
     found_paths = []
-    for word in wordlist:
+    for word in full_wordlist:
         target_url = f"{url}/{word}"
         response = requests.get(target_url)
         if response.status_code == 200:
             found_paths.append(target_url)
+    
     return found_paths
